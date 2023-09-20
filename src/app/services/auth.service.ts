ede1bb56d09f9e4629dff3ec45743b6cf044ff16
syrina gaddour <syrinagaddour11@gmail.com>
2023-09-20 18:10:39 +0100

    update

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,22 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-
+const apiUrl = environment.BackUrl;
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class AuthService {
     
   constructor(private http:HttpClient) { }
   
   Login(loginRequet: any) {
-    return this.http.post(`${environment.BackUrl}/users/login`, loginRequet)
+    return this.http.post(`${apiUrl}/users/login`, loginRequet)
   }
   Register(registerRequest: any) {
-    return this.http.post(`${environment.BackUrl}/users/candidat/register`, registerRequest)
+    return this.http.post(`${apiUrl}/users/candidat/register`, registerRequest)
   }
   Register_societe( id_category: any, id_city: any, registerRequest: any) {
-    return this.http.post(`${environment.BackUrl}/users/societe/register/${id_category}/${id_city}`, registerRequest)
+    return this.http.post(`${apiUrl}/users/societe/register/${id_category}/${id_city}`, registerRequest)
   
   }
 
